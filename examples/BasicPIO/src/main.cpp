735e7c33efe6b2613ea17a95161d4e53e853eb7a
Jan Havelka <jnhavelka@gmail.com>
2025-12-15 20:57:44 +0100

    stopwatch example

--- examples/BasicPIO/src/main.cpp
+++ examples/BasicPIO/src/main.cpp
@@ -7,6 +7,38 @@ static elapsedMillis64 heartbeat_ms(0);
 static elapsedMicros64 measurement_us(0);
 static Stopwatch stopwatch;
 
+static void handleSerialCommand(char cmd) {
+  switch (cmd) {
+    case 's': // start (reset + run)
+      stopwatch.start();
+      Serial.println("Stopwatch started");
+      break;
+    case 'p': // pause/stop
+      stopwatch.stop();
+      Serial.println("Stopwatch stopped");
+      break;
+    case 'r': // resume without clearing
+      stopwatch.resume();
+      Serial.println("Stopwatch resumed");
+      break;
+    case 'c': // clear but keep state
+      stopwatch.reset();
+      Serial.println("Stopwatch reset");
+      break;
+    case 't': // print elapsed
+      Serial.printf("Stopwatch elapsed: %lld ms (%s)\n",
+                    (long long)stopwatch.elapsedMillis(),
+                    formatTime(stopwatch.elapsedMicros()).c_str());
+      break;
+    case 'h':
+      Serial.println("Commands: s=start, p=stop, r=resume, c=reset, t=print, h=help");
+      break;
+    default:
+      Serial.printf("Unknown command '%c'. Send 'h' for help.\n", cmd);
+      break;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) {
@@ -34,6 +66,15 @@ void loop() {
                   (long long)elapsed,
                   (long long)stopwatch.elapsedMillis(),
                   formatTime(stopwatch.elapsedMicros()).c_str());
+  }
+
+  while (Serial.available() > 0) {
+    char cmd = (char)Serial.read();
+    // Ignore line endings
+    if (cmd == '\n' || cmd == '\r') {
+      continue;
+    }
+    handleSerialCommand(cmd);
   }
 
   delay(10);
